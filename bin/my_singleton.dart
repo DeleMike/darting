354e5487bda14f5a3d85416a43c733853efe7455
DeleMike <akindelemichael65@gmail.com>
2021-09-11 23:52:30 +0100

    singleton pattern

--- bin/my_singleton.dart
+++ bin/my_singleton.dart
@@ -1,4 +1,5 @@
 class MySingleton {
   MySingleton._();
-  static final MySingleton instance = MySingleton._();
+  static final MySingleton _instance = MySingleton._();
+  factory MySingleton() => _instance;
 }
--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -4,12 +4,18 @@ import 'user.dart';
 import 'my_singleton.dart';
 
 void main(List<String> arguments) {
-  final user = User.michael();
-  final json = user.toJson();
-  final deserializedUser = User.fromJson(json);
-  Log.out(deserializedUser.toString());
+  // final user = User.michael();
+  // final json = user.toJson();
+  // final deserializedUser = User.fromJson(json);
+  // Log.out(deserializedUser.toString());
 
   //SAMPLE
-  final singler = MySingleton.instance;
-  
+  final singler = MySingleton();
+  final singler2 = MySingleton();
+
+  if (singler == singler2) {
+    Log.out('true: same object');
+  } else {
+    Log.out('false: not same object');
+  }
 }
