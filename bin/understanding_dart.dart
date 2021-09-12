42af3bc76555d0215e270ff555aaaccc0a385d32
DeleMike <akindelemichael65@gmail.com>
2021-09-12 23:17:54 +0100

    null-challenges

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,3 +1,14 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  //CHALLENGE ONE
+  // var result = guessValue() ?? 0;
+  // print('Answer is: $result');
 }
+
+//CHALLENGE ONE
+// int? guessValue() {
+//   const values = [42, null];
+//   final r = Random();
+//   return values[r.nextInt(2)];
+// }
