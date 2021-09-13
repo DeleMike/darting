7d1164fab87bc256ee733fb4edac15ee01ae4d30
DeleMike <akindelemichael65@gmail.com>
2021-09-13 15:22:05 +0100

    trial one

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,21 +1,24 @@
 void main(List<String> arguments) {
-  // ListOperations.play();
-  ListOperations.add([1, 2, 3, 4]);
+  //ListOperations.play();
+  //ListOperations.add([1, 2, 3, 4]);
+  //ListOperations.firstChallenge('I code quite frequently');
+  ListOperations.secondChallenge('abbccbb');
 }
 
 class ListOperations {
   static void play() {
-    // var deserts = ['Cake', 'Egg-roll', 'Biscuits', 'Ice cream', 'Doughnut'];
-    // deserts.forEach(print);
+    var deserts = ['Cake', 'Egg-roll', 'Biscuits', 'Ice cream', 'Doughnut'];
+    print(deserts.reversed);
+
     // var bigDesert = [for (String desert in deserts) desert.toUpperCase()];
     // bigDesert.forEach(print);
 
-    final list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-    final unmodifiableList = List.unmodifiable(list);
-    list.add(4);
-    unmodifiableList.add(3);
-    print('List: $list');
-    print('unmodifiableList: $unmodifiableList');
+    // final list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    // final unmodifiableList = List.unmodifiable(list);
+    // list.add(4);
+    // unmodifiableList.add(3);
+    // print('List: $list');
+    // print('unmodifiableList: $unmodifiableList');
   }
 
   static void add(Iterable elements) {
@@ -25,4 +28,61 @@ class ListOperations {
     }
     print('Sum: $total');
   }
+
+  static void firstChallenge(String paragraph) {
+    var length = paragraph.length;
+    var unique = <String>{};
+    for (var i = 0; i < length; i++) {
+      if (paragraph[i] == ' ') {
+        continue;
+      }
+      unique.add(paragraph[i]);
+    }
+
+    print(unique);
+  }
+
+  //break into parts
+  static void secondChallenge(String paragraph) {
+    var length = paragraph.length;
+    var listOfStrings = <String>[];
+    var listOfLetters = <Letter>[];
+    var foundLetters = <String>[];
+    //var map = <String, int>{};
+
+    for (var i = 0; i < length; i++) {
+      listOfStrings.add(paragraph[i]);
+    }
+
+    for (var i = 0; i < listOfStrings.length; i++) {
+      for (var item in foundLetters) {
+        if (listOfStrings[i] == item) continue;
+      }
+
+      listOfLetters.add(Letter(
+          value: listOfStrings[i],
+          frequency: getCount(listOfStrings[i], listOfStrings)));
+      foundLetters.add(listOfStrings[i]);
+    }
+
+    print(listOfLetters);
+  }
+
+  static int getCount(String value, List<String> letters) {
+    var count = 0;
+    for (var letter in letters) {
+      if (value == letter) {
+        count++;
+      }
+    }
+    return count;
+  }
+}
+
+class Letter {
+  final String _value;
+  final int _frequency;
+  Letter({required String value, required int frequency})
+      : _value = value,
+        _frequency = frequency;
 }
