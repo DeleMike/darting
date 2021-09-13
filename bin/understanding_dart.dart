faba47da47dd8ff85ede45b0d891ef04a10899df
DeleMike <akindelemichael65@gmail.com>
2021-09-13 13:51:44 +0100

    learning collections

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,3 +1,28 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  // ListOperations.play();
+  ListOperations.add([1, 2, 3, 4]);
+}
+
+class ListOperations {
+  static void play() {
+    // var deserts = ['Cake', 'Egg-roll', 'Biscuits', 'Ice cream', 'Doughnut'];
+    // deserts.forEach(print);
+    // var bigDesert = [for (String desert in deserts) desert.toUpperCase()];
+    // bigDesert.forEach(print);
+
+    final list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    final unmodifiableList = List.unmodifiable(list);
+    list.add(4);
+    unmodifiableList.add(3);
+    print('List: $list');
+    print('unmodifiableList: $unmodifiableList');
+  }
+
+  static void add(Iterable elements) {
+    var total = 0;
+    for (int i in elements) {
+      total += i;
+    }
+    print('Sum: $total');
+  }
 }
