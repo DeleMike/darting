043ba3694df47de15c73250fda7273b4e76644f6
DeleMike <akindelemichael65@gmail.com>
2021-09-13 17:24:49 +0100

    initial setup

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,3 +1,28 @@
 void main(List<String> arguments) {
   print('Hello world!');
 }
+
+class Person {
+  String givenName;
+  String surname;
+
+  Person(this.givenName, this.surname);
+
+  String get fullName => '$givenName $surname';
+
+  @override
+  String toString() => fullName;
+}
+
+class Student {
+  String givenName;
+  String surname;
+  var grades = [];
+
+  Student(this.givenName, this.surname);
+
+  String get fullName => '$givenName $surname';
+
+  @override
+  String toString() => fullName;
+}
