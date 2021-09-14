180629918f658054b797df5cefea8ced692a1b6e
DeleMike <akindelemichael65@gmail.com>
2021-09-14 01:10:00 +0100

    create subclasses

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,6 +1,12 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final jane = Student('Jane', 'Snow');
+  final jessie = SchoolBandMember('Jessie', 'Jones');
+  final marty = StudentAthlete('Marty', 'McFly');
+
+  final students = [jane, jessie, marty];
 }
+
+enum Grade { A, B, C, D, E, F }
 
 class Person {
   String givenName;
@@ -14,15 +20,20 @@ class Person {
   String toString() => fullName;
 }
 
-class Student {
-  String givenName;
-  String surname;
-  var grades = [];
+class Student extends Person {
+  Student(String givenName, String surname) : super(givenName, surname);
 
-  Student(this.givenName, this.surname);
+  var grades = <Grade>[];
+}
 
-  String get fullName => '$givenName $surname';
+class SchoolBandMember extends Student {
+  SchoolBandMember(String givenName, String surname)
+      : super(givenName, surname);
+  static const minimumPracticeTime = 2;
+}
 
-  @override
-  String toString() => fullName;
+class StudentAthlete extends Student {
+  StudentAthlete(String givenName, String surname) : super(givenName, surname);
+
+  bool get isEligible => grades.every((grade) => grade != Grade.F);
 }
