4301476af53e993c48115bb904c1e9f775813026
DeleMike <akindelemichael65@gmail.com>
2021-09-18 13:51:48 +0100

    add assets

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,3 +1,13 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  // play();
+  stream();
 }
+
+void play() {
+  final myFuture = Future<int>.delayed(Duration(seconds: 1), () => 42)
+      .then((value) => print('Value: $value'))
+      .catchError((error) => print('Error: $error'))
+      .whenComplete(() => print('Future has completed'));
+}
+
+void stream() {}
