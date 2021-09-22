f51a15b451af7cce1a97eeb0e68e34c9cf0b7fbd
DeleMike <akindelemichael65@gmail.com>
2021-09-22 19:13:12 +0100

    file tasks

--- bin/understanding_dart.dart
+++ bin/understanding_dart.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
   // play();
   stream();
@@ -10,4 +12,26 @@ void play() {
       .whenComplete(() => print('Future has completed'));
 }
 
-void stream() {}
+Future<void> stream() async {
+  // final file = File('assets/text.txt');
+  // final contents = await file.readAsString();
+  // print(contents);
+
+  // //create a new file
+  // final newFile = File('assets/text_long.txt');
+  // //newFile.openWrite(mode: FileMode.append);
+
+  // for (var i = 0; i < 1000; i++) {
+  //   await newFile.writeAsString(contents, mode: FileMode.append);
+  // }
+  // print('done')
+  //
+  //
+  // ;
+
+  final file = File('assets/text_long.txt');
+  final stream = file.openRead();
+  stream.listen((data) {
+    print(data);
+  });
+}
